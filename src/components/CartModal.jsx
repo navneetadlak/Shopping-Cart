d3abb1dc305df29cb769c283010bec0de3738e72
Navneet Adlak <aniketadlak9@gmail.com>
2024-03-01 15:05:25 +0530

    improve

--- src/components/CartModal.jsx
+++ src/components/CartModal.jsx
@@ -6,7 +6,7 @@ function CartModal() {
   const { cart, total, clear, toggle } = useValue();
   return (
     <div className={styles.CartModal}>
-      <div className={styles.closeButton} onClick={toggl}>
+      <div className={styles.closeButton} onClick={toggle}>
         Close
       </div>
       <div className={styles.clearButton} onClick={clear}>
